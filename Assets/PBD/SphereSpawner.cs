ad0e4b35ece4dedf8c28013f7a8fc1d31cc80fd3
Steve Kane <kanesteven@gmail.com>
2018-08-02 11:05:44 -0500

    lots of garbo

---
+++ Assets/PBD/SphereSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+	public PBDSolver solver;
+	public PBDSphere SpherePrefab;
+	public float timeTillNextSpawn = 0f;
+	public float SpawnFrequency = 1f;
+
+	void FixedUpdate()
+	{
+		timeTillNextSpawn -= Time.fixedDeltaTime;
+
+		if (timeTillNextSpawn > 0)
+			return;
+
+		timeTillNextSpawn = SpawnFrequency;		
+
+		var sphere = Instantiate(SpherePrefab, transform.position + Random.onUnitSphere * 2f, transform.rotation);
+
+		Destroy(sphere.gameObject, 20f);
+	}
+}
---
+++ Assets/SphereCastTest/SphereCaster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SphereCaster : MonoBehaviour 
+{
+	public float r = .5f;
+	public float d = .5f;
+	public RaycastHit rh = new RaycastHit();
+	public Mesh mesh;
+
+	void OnDrawGizmos()
+	{
+		// Conclusion from these tests is that sphere casting does not detect ANYTHING that
+		// overlaps the initial sphere. You have to imagine that it will strike ONLY points that
+		// are not contained in the original sphere's volume.
+		var p = transform.position;
+		var dir = transform.right;
+		var didHit = Physics.SphereCast(p, r, dir, out rh, d);
+
+		Gizmos.color = didHit ? Color.green : Color.red;
+		Gizmos.DrawWireSphere(transform.position, r);
+		Gizmos.DrawWireSphere(transform.position + dir * d, r);
+	}
+}
